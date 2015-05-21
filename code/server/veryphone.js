1f51f7a75857dab456254243d809bfd0ed0d624d
n1k1tai <nicolas.lavelaine-de-maubeuge@voila.fr>
2015-05-21 11:31:57 +0200

    Added a user session

--- code/server/veryphone.js
+++ code/server/veryphone.js
@@ -1,8 +1,14 @@
-//Initializing the debug function :
+//Initializing the debug module :
 var debugModule = require("./veryphone/interface/debug.js");
 debug = new debugModule.debug(true, true, 'log.txt');
 debug.alertDebug("Veryphone server started", true, false, false);
 
-// Initializing the web server function :
+//Initializing the user session module :
+
+var userSessionModule = require("./veryphone/user_session/user_session.js");
+usersSessionModule.init();
+
+
+// Initializing the web server module :
 var webServerModule = require("./veryphone/webserver/webserver.js");
 webServerModule.init();
--- code/server/veryphone/user_session/user_session.js
+++ code/server/veryphone/user_session/user_session.js
@@ -1,123 +1,16 @@
 var debugModule = require('./../interface/debug');
 debug = new debugModule.debug(true, true, 'log.txt');
 
-// Uiid module for user identifying :
-var uuidModule = require('node-uuid');
-
-User.numberOfUsers=0;
-User.lastUserId=0;
-
-function User(cookie)
+function init()
 {
-	var uuid;
-	var lastActivity = new Date();
+	// Notifying server init :
+	debug.alertDebug("User session init", false, false);
 
-	debug.alertDebug("Creating a new user");
-	
-	User.numberOfUsers++;
-	uuid = uuidModule.v4();
-
-	// User description :
-
-	var registered = false;
-	var admin = false;
-
-
-	// if (typeof(cookieId != 'undefined'))
-	// {
-	// 	// Look for the cookie id in the database...
-
-	// }
-
-	this.getNumberOfUsers = function()
-	{
-		debug.alertDebug("We know have " + this.constructor.numberOfUsers + " users");	
-	}
-
-	this.getInactivityTimeS = function()
-	{
-		return Math.abs(new Date() - lastActivity);
-	}
-
-	this.setActiveNow = function()
-	{
-		lastActivity = new Date();
-	}
-
-	this.delete = function()
-	{
-		this.constructor.numberOfUsers--;
-		delete this;
-	}
-
-	debug.alertDebug("User id : created" )
+	var session = require('express-session');
 
 }
 
-function UserRamDatabase(maxNumberOfUsers)
-{
-	var userRamArray = new Array();
-	var numberOfUsersInRam = 0;
-	var maxNumberOfUsers = maxNumberOfUsers;
 
-	this.storeUser = function(user)
-	{
-		this.userRamArray[user.uuid] = user;
-		if (numberOfUsersInRam + 1 > maxNumberOfUsers) return false;
+exports.init = init;
 
-		userRamArray.push(user);
-		this.numberOfUsersInRam++;
-
-		debug.alertDebug("User uuid " + user.uuid + " sucessfully added to user session");
-		return true
-	}
-
-	this.getNumberOfUsersInRam = function()
-	{
-		return numberOfUsersInRam;
-	}
-
-	this.getUser = function(uuid)
-	{
-		return this.userRamArray[uuid];
-	}
-
-	this.deleteUser = function(uuid)
-	{
-		//this.userRamArray.splic
-	}
-
-	this.deleteInactiveUsers = function(inactivityTimeS)
-	{
-		for (var i=0, length = this.userRamArray.length; i < length; i++ )
-		{
-			if (this.userRamArray[i].getInactivityTimeS > inactivityTimeS)
-			{
-				debug.alertDebug("Deleting user id " + this.userRamArray[i].uuid + " because it was inactive", false, true);
-				this.userRamArray[i].delete();
-				this.userRamArray.splice(i,1);
-			}
-
-
-		}
-	}
-
-	this.delete = function()
-	{
-		delete userRamArray;
-		numberOfUsersInRam = 0;
-		delete this;
-	}
-
-}
-
-function UserAction(user, actionString)
-{
-	var User = user;
-	// TODO : continue
-}
-
-
-exports.User = User;
-exports.UserSession = UserRamDatabase;
 
