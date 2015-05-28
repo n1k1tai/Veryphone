441f8eee9d2612697fd09ebfe92c71e6ad9707a9
Tdeal du 38 <j.mathiron@gmail.com>
2015-05-28 09:14:55 +0200

    Test de la Regexp sur la connexion

--- code/website/getinput.js
+++ code/website/getinput.js
@@ -3,6 +3,17 @@
         $('#submit').on('click', function(e)
         {
             var data = {}
-            $.post("connexion", { email : $('#email').val(), password : $('#pwd').val() } );
+            var email = $('email').val();
+            function validateEmail(email) {
+
+			    var re = new regexp("/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i");
+			    return re.test(email);
+			}
+			if (re.test(email) == true) {
+            	$.post("connexion", { email : $('#email').val(), password : $('#pwd').val() } );
+            } 
+            else {
+            	$.post("connexion", { email : "Email invalide", password : "" } );
+            }
         });
     });
